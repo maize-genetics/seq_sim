6aba538adb8930ad9bd5f556b3826bfc1db13cd8
Brandon <brandon.monier@gmail.com>
2025-11-14 15:23:44 -0600

    Fix logging output

--- src/main/kotlin/net/maizegenetics/utils/LoggingUtils.kt
+++ src/main/kotlin/net/maizegenetics/utils/LoggingUtils.kt
@@ -3,8 +3,10 @@ package net.maizegenetics.utils
 import net.maizegenetics.Constants
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+import org.apache.logging.log4j.core.Appender
 import org.apache.logging.log4j.core.LoggerContext
 import org.apache.logging.log4j.core.appender.FileAppender
+import org.apache.logging.log4j.core.config.LoggerConfig
 import org.apache.logging.log4j.core.layout.PatternLayout
 import java.nio.file.Path
 import kotlin.io.path.createDirectories
@@ -30,6 +32,19 @@ object LoggingUtils {
         val context = LogManager.getContext(false) as LoggerContext
         val config = context.configuration
 
+        // Use a unique appender name based on the log file name
+        val appenderName = "FileAppender_${logFileName.replace(".", "_")}"
+
+        // Check if appender already exists and remove it
+        val existingAppender: Appender? = config.getAppender(appenderName)
+        if (existingAppender != null) {
+            config.rootLogger.removeAppender(appenderName)
+            val maizeLoggerConfig = config.getLoggerConfig("net.maizegenetics")
+            if (maizeLoggerConfig != null) {
+                maizeLoggerConfig.removeAppender(appenderName)
+            }
+        }
+
         val layout = PatternLayout.newBuilder()
             .withConfiguration(config)
             .withPattern(LOG_PATTERN)
@@ -39,14 +54,23 @@ object LoggingUtils {
             .withFileName(logFile.absolutePath)
             .withAppend(true)
             .withLocking(false)
-            .setName("WorkDirFileLogger")
+            .setName(appenderName)
             .setLayout(layout)
             .setConfiguration(config)
             .build()
 
         appender.start()
         config.addAppender(appender)
+
+        // Add to root logger
         config.rootLogger.addAppender(appender, null, null)
+
+        // Also add to the net.maizegenetics logger (which has additivity=false)
+        val maizeLogger = config.getLoggerConfig("net.maizegenetics")
+        if (maizeLogger != null) {
+            maizeLogger.addAppender(appender, null, null)
+        }
+
         context.updateLoggers()
 
         logger.info("Logging to file: $logFile")
