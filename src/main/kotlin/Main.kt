41b5c13ef64a092d17528e0c740ecc89e7611c8f
Brandon <brandon.monier@gmail.com>
2025-11-15 17:42:05 -0600

    Add subcommands

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,8 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.main
 import com.github.ajalt.clikt.core.subcommands
 import net.maizegenetics.commands.AlignAssemblies
+import net.maizegenetics.commands.DownsampleGvcf
+import net.maizegenetics.commands.ExtractChromIds
 import net.maizegenetics.commands.MafToGvcf
 import net.maizegenetics.commands.SetupEnvironment
 
@@ -12,5 +14,5 @@ class SeqSim : CliktCommand() {
 }
 
 fun main(args: Array<String>) = SeqSim()
-    .subcommands(SetupEnvironment(), AlignAssemblies(), MafToGvcf())
+    .subcommands(SetupEnvironment(), AlignAssemblies(), MafToGvcf(), DownsampleGvcf(), ExtractChromIds())
     .main(args)
